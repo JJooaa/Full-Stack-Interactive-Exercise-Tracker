dbb7b385f1692e98dd4f6308516e23e854795de7
Joa <leitola.joa@gmail.com>
2021-06-01 14:18:55 +0300

    fix uri in api.js

--- src/Util/Api.js
+++ src/Util/Api.js
@@ -2,18 +2,18 @@ import axios from "axios";
 
 export const deleteExercise = async (id) => {
     const res = axios
-        .delete(`http://localhost:5000/exercises/${id}`)
+        .delete(`https://joanodeback.herokuapp.com/exercises/${id}`)
     return (await res).data;
 };
 
 export const postExercise = async (exercise) => {
     const res = axios
-        .post("http://localhost:5000/exercises/add", exercise)
+        .post("https://joanodeback.herokuapp.com/exercises/add", exercise)
         
     return (await res).data;
 };
 
 export const fetchExercises = async () => {
-    const res = await axios.get("http://localhost:5000/exercises/");
+    const res = await axios.get("https://joanodeback.herokuapp.com/exercises");
     return res.data;
 };
